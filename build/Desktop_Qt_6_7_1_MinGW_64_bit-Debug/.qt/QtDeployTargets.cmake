aed9487fcc8fe3a69a4dadbf264613f03fa54d1c
mingpuwu <mingpuwumail@163.com>
2024-06-01 10:06:49 +0800

    delete remote file

--- build/Desktop_Qt_6_7_1_MinGW_64_bit-Debug/.qt/QtDeployTargets.cmake
+++
@@ -1,2 +0,0 @@
-set(__QT_DEPLOY_TARGET_RDPBoost_FILE C:/Users/mingp/Documents/RDPBoost/build/Desktop_Qt_6_7_1_MinGW_64_bit-Debug/RDPBoost.exe)
-set(__QT_DEPLOY_TARGET_RDPBoost_RUNTIME_DLLS D:/InstallPack/QT/6.7.1/mingw_64/bin/Qt6Widgets.dll;D:/InstallPack/QT/6.7.1/mingw_64/bin/Qt6Gui.dll;D:/InstallPack/QT/6.7.1/mingw_64/bin/Qt6Core.dll)
